2f4e7e076878624b5b95f468bf0822ae5e869de0
wangqi <774948439@qq.com>
2017-05-05 17:49:19 +0000

    修改raw-data里的蛋糕价格与重量

--- server/tools/fixture/raw-data.js
+++ server/tools/fixture/raw-data.js
@@ -5,7 +5,7 @@ export default {
     intro:'瑞士防潮糖粉、新西兰安佳奶油芝士、美雅士朗姆酒、奥利奥坚果底坯',
     style:'提拉米苏',
     image:'image/cakelist/01.jpg',
-    price:188,
+    price:109,
     size:'2磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
@@ -16,7 +16,7 @@ export default {
     intro:'澳洲Binder Vally缤堡谷的“缤酸奶”、纯鲜时令水果',
     style:'缤果雪慕',
     image:'image/cakelist/02.jpg',
-    price:188,
+    price:152,
     size:'2磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
@@ -28,8 +28,8 @@ export default {
     intro:'美国大杏仁片、法国铁塔淡奶油、时令鲜水果',
     style:'彩虹巴黎',
     image:'image/cakelist/03.jpg',
-    price:188,
-    size:'2磅',
+    price:118,
+    size:'1.5磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
 
@@ -40,7 +40,7 @@ export default {
     intro:'纯鲜芒果、法国宝茸顶级芒果果蓉、法国铁塔淡奶油',
     style:'芒果慕斯',
     image:'image/cakelist/04.jpg'
-    ,price:188,
+    ,price:222,
     size:'2磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
@@ -52,8 +52,8 @@ export default {
     intro:'瑞士防潮糖粉、新西兰安佳奶油芝士、美雅士朗姆酒、奥利奥坚果底坯',
     style:'夏威夷',
     image:'image/cakelist/05.jpg',
-    price:188,
-    size:'2磅',
+    price:128,
+    size:'1.5磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
 
@@ -64,7 +64,7 @@ export default {
     intro:'瑞士防潮糖粉、新西兰安佳奶油芝士、美雅士朗姆酒、奥利奥坚果底坯',
     style:'草莓慕斯',
     image:'image/cakelist/06.jpg',
-    price:188,
+    price:201,
     size:'2磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
@@ -76,8 +76,8 @@ export default {
     intro:'澳洲Binder Vally缤堡谷的“缤酸奶”、纯鲜时令水果',
     style:'缤果雪慕',
     image:'image/cakelist/07.jpg',
-    price:188,
-    size:'2磅',
+    price:99,
+    size:'1磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
 
@@ -88,8 +88,8 @@ export default {
     intro:'美国大杏仁片、法国铁塔淡奶油、时令鲜水果',
     style:'彩虹巴黎',
     image:'image/cakelist/08.jpg',
-    price:188,
-    size:'2磅',
+    price:158,
+    size:'1磅',
     infore:'第一次见到你，我的目光已不由自主的被你占据，我的这颗心将被你征服，当你喊出我的名字时，我知道你是人群中最特别的，现在的我如此渴望能够与你再次相逢。',
     inforc:'You captured my heart and soul at the first sight. When you call my name, I hear the sweetest voice singing the song of nature. I long for another chance to see you and to hear you again.'
 
